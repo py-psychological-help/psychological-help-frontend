9614bd4d87314df7bb340adb7b9692e82d1e6b0e
Anna Maltseva <mr-sbazin@yandex.ru>
2024-01-11 10:40:26 +0300

    style: change chats styles

--- src/components/Chat/Chat.jsx
+++ src/components/Chat/Chat.jsx
@@ -1,23 +1,29 @@
-// import React from 'react';
-// import {NavLink} from 'react-router-dom';
-// import PropTypes from 'prop-types';
-// import cls from './Chat.module.scss';
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import cls from './Chat.module.scss';
 
-// function Chat({id, client}) {
-//     return (
-//         <div className={cls.chat}>
-//             <h2 className={cls.chatMessage}>{id}</h2>
-//             <NavLink to="/messages" className={cls.chatButton}>
-//                 Подключиться
-//             </NavLink>
-//         </div>
-//     );
-// }
+// eslint-disable-next-line react/prop-types
+function Chat({ id, firstName, complaint }) {
+	return (
+		<div className={cls.chat}>
+			<div className={cls.chatInfo}>
+				<h2 className={cls.number}>{`Заявка №${id}`}</h2>
+				<p className={cls.name}>{`Имя: ${firstName}`}</p>
+				<p className={cls.problem}>{`Проблема: ${complaint}`}</p>
+			</div>
+			<NavLink to="/messages" className={cls.chatButton}>
+				Подключиться
+			</NavLink>
+		</div>
+	);
+}
 
-// Chat.propTypes = {
-//     name: PropTypes.string.isRequired,
-//     number: PropTypes.number.isRequired,
+Chat.propTypes = {
+	id: PropTypes.number.isRequired,
+	firstName: PropTypes.string.isRequired,
+	complaint: PropTypes.string.isRequired,
+	// client: PropTypes.oneOfType([PropTypes.object]).isRequired,
+};
 
-// };
-
-// export default Chat;
+export default Chat;
--- src/components/Chats/Chats.jsx
+++ src/components/Chats/Chats.jsx
@@ -1,20 +1,29 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom';
 import cls from './Chats.module.scss';
 import Chat from '../Chat/Chat';
 
 const chats = [
 	{
-		id: 10,
-		name: 'Проблема первые 5 слов.. в 1 строку',
+		client: {
+			first_name: 'Анастасия',
+			complaint: 'Психосоматика / физическое здоровье',
+		},
+		id: 42,
+	},
+
+	{
+		client: {
+			first_name: 'User',
+			complaint: 'Семейные проблемы',
+		},
+		id: 43,
 	},
 	{
-		id: 5,
-		name: 'Проблема первые 5 слов.. в 1 строку',
-	},
-	{
-		id: 3,
-		name: 'Проблема первые 5 слов.. в 1 строку',
+		client: {
+			first_name: '',
+			complaint: 'Я не знаю что со мной',
+		},
+		id: 44,
 	},
 ];
 
@@ -36,38 +45,14 @@ function Chats() {
 				</li>
 			</ul>
 			<ul className={cls.chatsList}>
-				<li className={cls.chatItem}>
-					<h2 className={cls.chatNumber}>Заявка №42</h2>
-					<p className={cls.name}>Имя: Анастасия</p>
-					<p className={cls.problem}>
-						Проблема: Психосоматика / физическое здоровье
-					</p>
-					<NavLink to="/messages" className={cls.chatButton}>
-						Подключиться
-					</NavLink>
-				</li>
-				<li className={cls.chatItem}>
-					<h2 className={cls.chatNumber}>Заявка №43</h2>
-					<p className={cls.name}>Имя: User</p>
-					<p className={cls.problem}>Проблема: Семейные проблемы</p>
-					<NavLink to="/messages" className={cls.chatButton}>
-						Подключиться
-					</NavLink>
-				</li>
-				<li className={cls.chatItem}>
-					<h2 className={cls.chatNumber}>Заявка №44</h2>
-					<p className={cls.name} />
-					<p className={cls.problem}>
-						Проблема: Я не знаю что со мной
-					</p>
-					<NavLink to="/messages" className={cls.chatButton}>
-						Подключиться
-					</NavLink>
-				</li>
-
-				{/* {chats.map((chat) => (
-                        <Chat key={chat.id} id={chat.id} name={chat.name}/>
-                    ))} */}
+				{chats.map((chat) => (
+					<Chat
+						key={chat.id}
+						id={chat.id}
+						firstName={chat.client.first_name}
+						complaint={chat.client.complaint}
+					/>
+				))}
 			</ul>
 		</main>
 	);
