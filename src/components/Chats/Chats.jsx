516cf86f7a27dc644b4ffaa7cfe70842a365df35
Anna Maltseva <mr-sbazin@yandex.ru>
2024-01-14 17:06:08 +0300

    fix: close-button, dashboard

--- src/components/Chats/Chats.jsx
+++ src/components/Chats/Chats.jsx
@@ -1,23 +1,55 @@
-import React from 'react';
+import React, { useState } from 'react';
+import clsx from 'clsx';
 import cls from './Chats.module.scss';
 import Chat from '../Chat/Chat';
 import chats from '../../utils/chats';
 
-function Chats() {
+function Chats(props) {
+	const [isActive, setIsActive] = useState(0);
+
+	const { className } = props;
+
 	return (
 		<main className={cls.chats}>
 			<h1 className={cls.title}>Чаты</h1>
 			<ul className={cls.dashboard}>
 				<li>
-					<button className={`${cls.dashboardItem} ${cls.active}`}>
+					<button
+						type="button"
+						id={1}
+						onClick={() => {
+							setIsActive(1);
+						}}
+						className={clsx(cls.dashboardItem, className, {
+							[cls.active]: isActive === 1,
+						})}
+					>
 						Ожидающие
 					</button>
 				</li>
 				<li>
-					<button className={cls.dashboardItem}>Активные</button>
+					<button
+						type="button"
+						id={2}
+						onClick={() => setIsActive(2)}
+						className={clsx(cls.dashboardItem, className, {
+							[cls.active]: isActive === 2,
+						})}
+					>
+						Активные
+					</button>
 				</li>
 				<li>
-					<button className={cls.dashboardItem}>Архив</button>
+					<button
+						type="button"
+						id={3}
+						onClick={() => setIsActive(3)}
+						className={clsx(cls.dashboardItem, className, {
+							[cls.active]: isActive === 3,
+						})}
+					>
+						Архив
+					</button>
 				</li>
 			</ul>
 			<ul className={cls.chatsList}>
