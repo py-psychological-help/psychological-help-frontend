044c75eb2ba1d249e316406fd0fe0e513f28256f
EZzzKryak <manicynaleksandr@yandex.ru>
2024-01-19 12:45:00 +0300

    fix(refactoring): fix styles and fonst, delete unused components

--- src/pages/AboutProjectPage/AboutProjectPage.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import AboutProject from '../../components/AboutProject/AboutProject';
-
-const AboutProjectPage = () => {
-	return <AboutProject />;
-};
-
-export default AboutProjectPage;
